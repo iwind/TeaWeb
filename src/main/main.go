b65bfd20ce576ea79845784a832ec81fe12cb205
刘祥超 <iwind.liu@gmail.com>
2018-10-09 18:04:07 +0800

    增加超时重试功能

--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/iwind/TeaWebCode/teaweb"
+import (
+	"github.com/iwind/TeaWebCode/teaweb"
+	"net/http"
+	_ "net/http/pprof"
+)
 
 func main() {
+	go http.ListenAndServe("0.0.0.0:8080", nil)
+
 	teaweb.Start()
 }
